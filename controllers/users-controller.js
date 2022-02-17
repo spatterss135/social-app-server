c2738f11bede0f5b0b89925d6e8b06bff60327f9
jhersbo <jhersbo@gmail.com>
2022-02-17 15:52:20 -0600

    work on users controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,14 +1,69 @@
-const users = require('express').Router()
+const router = require('express').Router()
 const db = require('../models')
-const {User} = db;
+const { Op } = require('sequelize')
+const { users, posts, friends } = db;
 
 //get all users
-users.get('/', (req, res)=> {
-    res.send('User Page')
+router.get('/', async (req, res)=> {
+    try{
+        const foundUsers = await users.findAll()
+        res.status(200).json(foundUsers)
+    }
+    catch(err){
+        res.status(500).json(err)
+        console.log(err)
+    }
+})
+
+//add a user
+router.post('/', (req, res)=>{
+    res.send('Add a new user')
+})
+
+//edit a user
+router.put('/', (req, res)=>{
+    res.send('Edit a user')
+})
+
+//delete a user
+router.delete('/', (req, res)=>{
+    res.send('Delete a user')
 })
 
 //get a specific user
+router.get('/:name', async (req, res)=>{
+    try{
+        const foundUsers = await users.findOne({
+            where: {
+                name: req.params.name
+            },
+            include: [
+                {
+                    model: posts,
+                    as: "posts",
+                    attributes:{
+                        exclude: "user_id"
+                    }
+                },
+                {
+                    model: friends,
+                    as: "friends",
+                    attributes:{
+                        exclude: ['user_id', 'friendship_id']
+                    }
+                }
+            ]
+        })
+        res.status(200).json(foundUsers)
+    }
+    catch(err){
+        res.status(500).json(err)
+        console.log(err)
+    }
+})
 
-//get 
+//get a user's posts
 
-module.exports = users;
+
+
+module.exports = router;
--- seeders/20220217025404-friends.js
+++ seeders/20220217025404-friends.js
@@ -4,30 +4,36 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     queryInterface.bulkInsert('friends', [
       {
+        friendship_id: 1,
         user_id: 1,
         friend_id: 2
       },
       {
+        friendship_id: 2,
         user_id: 1,
         friend_id: 3
       },
       {
+        friendship_id: 3,
         user_id: 1,
         friend_id: 4
       },
       {
+        friendship_id: 4,
         user_id: 2,
         friend_id: 3
       },
       {
+        friendship_id: 5,
         user_id: 2,
         friend_id: 4
       },
       {
+        friendship_id: 6,
         user_id: 3,
         friend_id: 4
       }
-    ])
+    ], {})
   },
 
   async down (queryInterface, Sequelize) {
