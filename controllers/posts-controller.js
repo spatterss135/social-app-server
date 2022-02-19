23064fd741784b828abc0a56670705ad75ea1a43
spatterss135 <spatterss135@gmail.com>
2022-02-19 10:51:44 -0600

    Added await to functions that needed it

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res)=> {
 //create a post
 router.post('/', async (req, res)=>{
     try{
-        posts.create(req.body)
+        await posts.create(req.body)
         res.status(200).json('Post created.')
     }
     catch(err){
@@ -24,7 +24,7 @@ router.post('/', async (req, res)=>{
 //edit a post
 router.put('/:post_id', async (req, res)=>{
     try{
-        posts.update(req.body, {
+        await posts.update(req.body, {
             where:{
                 post_id: req.params.post_id
             }
@@ -39,7 +39,7 @@ router.put('/:post_id', async (req, res)=>{
 //delete a post
 router.delete('/:post_id', async (req, res)=>{
     try{
-        posts.destroy({
+        await posts.destroy({
             where:{
                 post_id: req.params.post_id
             }
