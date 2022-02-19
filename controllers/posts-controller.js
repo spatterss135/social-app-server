29e0c1c48daeebac6a13cb258957f01cfd0e488c
jhersbo <jhersbo@gmail.com>
2022-02-19 10:11:57 -0600

    can make new posts

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,15 +1,28 @@
-const postsRouter = require('express').Router()
+const router = require('express').Router()
 const db = require('../models')
-const {posts} = db;
+const { posts } = db;
 const { Op } = require("sequelize");
+const res = require('express/lib/response');
 
-
-postsRouter.get('/', async (req, res)=> {
+//get all posts
+router.get('/', async (req, res)=> {
     let postsInDatabase = await posts.findAll()
     res.send(postsInDatabase)
 })
 
-postsRouter.get('/:friends', async (req, res)=> {
+//create a post
+router.post('/', async (req, res)=>{
+    try{
+        posts.create(req.body)
+        res.status(200).json('Post created.')
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//get posts made by friends
+router.get('/:friends', async (req, res)=> {
     let friends = req.params.friends.split(',').map(num => Number(num))
     let postsInDatabaseMadeByFriends = await posts.findAll({
         where: {
@@ -21,4 +34,4 @@ postsRouter.get('/:friends', async (req, res)=> {
     res.send(postsInDatabaseMadeByFriends)
 })
 
-module.exports = postsRouter;
+module.exports = router;
