feeff874d270f9199be9e294b0f6344c2561464f
jhersbo <jhersbo@gmail.com>
2022-02-21 20:12:25 -0600

    forgot one await

--- controllers/likes-controller.js
+++ controllers/likes-controller.js
@@ -6,7 +6,7 @@ const { likes, posts } = db
 //Like a post
 router.post('/', async (req, res)=>{
     try{
-        likes.create(req.body)
+        await likes.create(req.body)
         res.status(200).json('Post liked')
     }
     catch(err){
