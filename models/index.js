355f05d97dd27c13648108d7fbccf5b547c71e39
spatterss135 <spatterss135@gmail.com>
2022-02-26 13:38:03 -0600

    fff

--- models/index.js
+++ models/index.js
@@ -9,16 +9,16 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-}
-  else if (process.env.DATABASE_URL){
-    sequelize = new Sequelize(process.env.DATABASE_URL, config);
-  }
-else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// }
+//   else if (process.env.DATABASE_URL){
+//     sequelize = new Sequelize(process.env.DATABASE_URL, config);
+//   }
+// else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+sequelize = new Sequelize(process.env.DATABASE_URL, {"dialectOptions": {"ssl": {"require": true, "rejectUnauthorized": false}}});
 fs
   .readdirSync(__dirname)
   .filter(file => {
