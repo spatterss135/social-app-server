ed4c683e81e31ea705fb1be455dce84cb66c6c99
spatterss135 <spatterss135@gmail.com>
2022-02-28 18:53:36 -0600

    Added configs for heroku

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -32,49 +32,49 @@ router.get('/', async (req, res)=> {
 })
 
 // //create a post
-router.post('/', async (req, res, next)=>{
+// router.post('/', async (req, res, next)=>{
     
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, './uploads')
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.originalname)
+//     const storage = multer.diskStorage({
+//         destination: function (req, file, cb) {
+//           cb(null, './uploads')
+//         },
+//         filename: function (req, file, cb) {
+//             cb(null, file.originalname)
 
-        }
-      })
+//         }
+//       })
       
-      const upload = multer({ storage: storage })
-    //   upload(req, res, error => {if (error) throw error}
-    //   )
-    //   next()
-    return upload.single('photo')(req, res, next)
-}, 
+//       const upload = multer({ storage: storage })
+//     //   upload(req, res, error => {if (error) throw error}
+//     //   )
+//     //   next()
+//     return upload.single('photo')(req, res, next)
+// }, 
 
-async (req, res)=>{
+// async (req, res)=>{
     
-    let photoKey = `https://socialappphotosbucket.s3.us-east-2.amazonaws.com/${req.file.originalname}_${Date.now().toString()}`
-    try{
-        // 
-        fs.readFile(`./uploads/${req.file.originalname}`,
-        (err, data) => {
-            if (err) throw err
-            s3.upload({Bucket: 'socialappphotosbucket', Key: photoKey, 
-        Body: data}, (err) => {if (err) throw err})
-        })
+//     let photoKey = `https://socialappphotosbucket.s3.us-east-2.amazonaws.com/${req.file.originalname}_${Date.now().toString()}`
+//     try{
+//         // 
+//         fs.readFile(`./uploads/${req.file.originalname}`,
+//         (err, data) => {
+//             if (err) throw err
+//             s3.upload({Bucket: 'socialappphotosbucket', Key: photoKey, 
+//         Body: data}, (err) => {if (err) throw err})
+//         })
 
-        await posts.create({user_id: req.body.user_id, content: req.body.content, image: photoKey})
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-    res.end()
-})
-
-// router.post('/', async (req, res) => {
-//     await posts.create(req.body)
-//     res.status(200).json('Friendship created')
+//         await posts.create({user_id: req.body.user_id, content: req.body.content, image: photoKey})
+//     }
+//     catch(err){
+//         res.status(500).json(err)
+//     }
+//     res.end()
 // })
+
+router.post('/', async (req, res) => {
+    await posts.create(req.body)
+    res.status(200).json('Friendship created')
+})
 
 //edit a post
 router.put('/:post_id', async (req, res)=>{
